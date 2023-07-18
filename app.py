288d4ab8e998a7a198f3dc7c896b6b8ce15a809c
jmazcunan <jotajota46@gmail.com>
2023-07-18 21:04:38 +0200

    creation

---
+++ app.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+import streamlit.components.v1 as components
+
+st.set_page_config(page_title="Padel Pose", page_icon="🎾", layout="centered", initial_sidebar_state="auto", menu_items=None)
