73ce1bdb0212218faad07d0ea130eff77c6d3f2a
jmazcunan <77403471+jmazcunan@users.noreply.github.com>
2023-07-20 10:21:39 +0200

    Update helper.py

--- helper.py
+++ helper.py
@@ -5,6 +5,10 @@ import dataclasses
 from typing import List, Mapping, Optional, Tuple, Union
 import requests
 import os
+#import plotly.express as px
+import math
+import cv2
+
 
 _PRESENCE_THRESHOLD = 0.5
 _VISIBILITY_THRESHOLD = 0.5
@@ -31,8 +35,106 @@ def download(url, filename):
         r = requests.get(url, allow_redirects=True)
         open(filename, 'wb').write(r.content)
 
+def _normalized_to_pixel_coordinates(
+    normalized_x: float, normalized_y: float, image_width: int,
+    image_height: int) -> Union[None, Tuple[int, int]]:
+  """Converts normalized value pair to pixel coordinates."""
 
-def draw_landmarks_on_image(rgb_image, detection_result):
+  # Checks if the float value is between 0 and 1.
+  def is_valid_normalized_value(value: float) -> bool:
+    return (value > 0 or math.isclose(0, value)) and (value < 1 or
+                                                      math.isclose(1, value))
+
+  if not (is_valid_normalized_value(normalized_x) and
+          is_valid_normalized_value(normalized_y)):
+    # TODO: Draw coordinates even if it's outside of the image bounds.
+    return None
+  x_px = min(math.floor(normalized_x * image_width), image_width - 1)
+  y_px = min(math.floor(normalized_y * image_height), image_height - 1)
+  return x_px, y_px
+
+def draw_landmark_custom(
+    image: np.ndarray, 
+    landmark_ids,
+    landmark_list: landmark_pb2.NormalizedLandmarkList,
+    connections: Optional[List[Tuple[int, int]]] = None,
+    landmark_drawing_spec: Union[DrawingSpec,
+                                 Mapping[int, DrawingSpec]] = DrawingSpec(
+                                     color=RED_COLOR),
+    connection_drawing_spec: Union[DrawingSpec,
+                                   Mapping[Tuple[int, int],
+                                           DrawingSpec]] = DrawingSpec()):
+  """Draws the landmarks and the connections on the image.
+
+  Args:
+    image: A three channel BGR image represented as numpy ndarray.
+    landmark_list: A normalized landmark list proto message to be annotated on
+      the image.
+    connections: A list of landmark index tuples that specifies how landmarks to
+      be connected in the drawing.
+    landmark_drawing_spec: Either a DrawingSpec object or a mapping from hand
+      landmarks to the DrawingSpecs that specifies the landmarks' drawing
+      settings such as color, line thickness, and circle radius. If this
+      argument is explicitly set to None, no landmarks will be drawn.
+    connection_drawing_spec: Either a DrawingSpec object or a mapping from hand
+      connections to the DrawingSpecs that specifies the connections' drawing
+      settings such as color and line thickness. If this argument is explicitly
+      set to None, no landmark connections will be drawn.
+
+  Raises:
+    ValueError: If one of the followings:
+      a) If the input image is not three channel BGR.
+      b) If any connetions contain invalid landmark index.
+  """
+  if not landmark_list:
+    return
+  if image.shape[2] != _BGR_CHANNELS:
+    raise ValueError('Input image must contain three channel bgr data.')
+  image_rows, image_cols, _ = image.shape
+  idx_to_coordinates = {}
+  for idx, landmark in enumerate(landmark_list.landmark):
+    if ((landmark.HasField('visibility') and
+         landmark.visibility < _VISIBILITY_THRESHOLD) or
+        (landmark.HasField('presence') and
+         landmark.presence < _PRESENCE_THRESHOLD)):
+      continue
+    landmark_px = _normalized_to_pixel_coordinates(landmark.x, landmark.y,
+                                                   image_cols, image_rows)
+    if landmark_px:
+      idx_to_coordinates[idx] = landmark_px
+  # if connections:
+  #   num_landmarks = len(landmark_list.landmark)
+  #   # Draws the connections if the start and end landmarks are both visible.
+  #   for connection in connections:
+  #     start_idx = connection[0]
+  #     end_idx = connection[1]
+  #     if not (0 <= start_idx < num_landmarks and 0 <= end_idx < num_landmarks):
+  #       raise ValueError(f'Landmark index is out of range. Invalid connection '
+  #                        f'from landmark #{start_idx} to landmark #{end_idx}.')
+  #     if start_idx in idx_to_coordinates and end_idx in idx_to_coordinates:
+  #       drawing_spec = connection_drawing_spec[connection] if isinstance(
+  #           connection_drawing_spec, Mapping) else connection_drawing_spec
+  #       cv2.line(image, idx_to_coordinates[start_idx],
+  #                idx_to_coordinates[end_idx], drawing_spec.color,
+  #                drawing_spec.thickness)
+  # Draws landmark points after finishing the connection lines, which is
+  # aesthetically better.
+  if landmark_drawing_spec:
+    for idx, landmark_px in idx_to_coordinates.items():
+
+      if idx in landmark_ids:  ### ONLY DRAW SELECTED LANDMARKS
+        drawing_spec = landmark_drawing_spec[idx] if isinstance(
+            landmark_drawing_spec, Mapping) else landmark_drawing_spec
+        # White circle border
+        circle_border_radius = max(drawing_spec.circle_radius + 1,
+                                  int(drawing_spec.circle_radius * 1.2))
+        cv2.circle(image, landmark_px, circle_border_radius, WHITE_COLOR,
+                  drawing_spec.thickness)
+        # Fill color into the circle
+        cv2.circle(image, landmark_px, drawing_spec.circle_radius,
+                  drawing_spec.color, drawing_spec.thickness)
+      
+def draw_landmarks_on_image(rgb_image, detection_result, landmark_id=None):
   pose_landmarks_list = detection_result.pose_landmarks
   annotated_image = np.copy(rgb_image)
 
@@ -45,11 +147,20 @@ def draw_landmarks_on_image(rgb_image, detection_result):
     pose_landmarks_proto.landmark.extend([
       landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y, z=landmark.z) for landmark in pose_landmarks
     ])
-    solutions.drawing_utils.draw_landmarks(
-      annotated_image,
-      pose_landmarks_proto,
-      solutions.pose.POSE_CONNECTIONS,
-      solutions.drawing_styles.get_default_pose_landmarks_style())
+
+    if landmark_id == None:
+      solutions.drawing_utils.draw_landmarks(
+        annotated_image,
+        pose_landmarks_proto,
+        solutions.pose.POSE_CONNECTIONS,
+        solutions.drawing_styles.get_default_pose_landmarks_style())
+    else:
+      draw_landmark_custom(
+        annotated_image, landmark_id,
+        pose_landmarks_proto,
+        solutions.pose.POSE_CONNECTIONS,
+        solutions.drawing_styles.get_default_pose_landmarks_style())
+      
   return annotated_image
 
 
